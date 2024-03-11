a36b1007b0d4a2b1f1ea77e14aec0cdb4c16c1c3
dukehh <duke.germany@gmail.com>
2024-03-11 22:17:31 +0100

    duke_commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 
+
 def timeit(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
@@ -8,14 +9,14 @@ def timeit(func):
         execution_time = end_time - start_time
         print(f"Execution time: {execution_time} seconds")
         return result
+
     return wrapper
+
 
 @timeit
 def my_function(sec):
     time.sleep(sec)
 
 
-
 if __name__ == "__main__":
     my_function(2)
-    
